0b7ef6d5d61883834dcdeadf13e5c9a9c442e04e
Harry <harry@Harrys-MBP.home>
2025-12-11 11:36:48 +1100

    Fix catch-all route breaking static asset serving

--- backend/doubletake/urls.py
+++ backend/doubletake/urls.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from frontend.views import home
 
 urlpatterns = [
-    # Django admin
     path("admin/", admin.site.urls),
-    
-    # Root path
     path("", home, name="home"),
-    
-    # Catch-all for React Router (SPA routing) - serves index.html for all other routes
-    # This must be last - excludes admin and static paths
-    re_path(r'^(?!admin|static).+$', home, name="home-catchall"),
 ]
-
-# Note: Static files (JS, CSS, assets) are served by WhiteNoise
-# They should be in STATIC_ROOT after running collectstatic
